4d24de8df04e16cca6dd19c4ce19195bdda24ad1
san <sanandmv@duck.com>
2024-11-16 14:51:43 +0530

    add prover contract

--- packages/contracts/src/ZuCitizenshipRegistry.sol
+++ packages/contracts/src/ZuCitizenshipRegistry.sol
@@ -1,7 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-contract ZuCitizenshipRegistry {
+import {Strings} from "@openzeppelin-contracts-5.0.1/utils/Strings.sol";
+import {Prover} from "vlayer-0.1.0/src/Prover.sol";
+import {VerifiedEmail, UnverifiedEmail, EmailProofLib} from "vlayer-0.1.0/src/EmailProof.sol";
+import {EmailStrings} from "./EmailStrings.sol";
+
+contract ZuCitizenshipRegistry is Prover {
+    using Strings for string;
+    using EmailStrings for string;
+    using EmailProofLib for UnverifiedEmail;
+
     // Struct to store citizen information
     struct Citizen {
         bool isCitizen; // Indicates if the address is a registered citizen
@@ -18,6 +27,30 @@ contract ZuCitizenshipRegistry {
     event IdentityAdded(address indexed citizen, address identity);
     event IdentityRemoved(address indexed citizen, address identity);
 
+    string targetDomain;
+
+    constructor(string memory _targetDomain) {
+        targetDomain = _targetDomain;
+    }
+
+    // TODO: verify receiver
+    function main(
+        UnverifiedEmail calldata unverifiedEmail,
+        address targetWallet
+    ) public view returns (Proof, bytes32, address) {
+        VerifiedEmail memory email = unverifiedEmail.verify();
+
+        require(email.from.contains(targetDomain), "incorrect sender domain");
+        require(
+            email.subject.equal(
+                "[POAP Inside] Thanks for hacking at ETHGlobal Singapore 2024!"
+            ),
+            "incorrect subject"
+        );
+
+        return (proof(), sha256(abi.encodePacked(email.from)), targetWallet);
+    }
+
     // Function to register a new citizen
     // Only the admin can call this function
     // _citizen: Address of the new citizen
@@ -29,5 +62,4 @@ contract ZuCitizenshipRegistry {
         citizens[_citizen].identities.push(_citizen); // Add the citizen's address as their first identity
         emit CitizenRegistered(_citizen, _expiration);
     }
-
 }
